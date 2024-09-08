234217f435ef0235e49cf4be61fc2ecdc0c481fb
Ridwan2638 <sanniridwanolamide@gmail.com>
2024-09-08 15:05:27 +0100

    update the vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio",
+  base: "/Ridwan2638.github.io",
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+  },
 })
